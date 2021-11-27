04c5120b19d44cfe98be47f3cc2021d54782c13d
HIEU LE DINH <HIEU.LD183530@sis.hust.edu.vn>
2021-11-28 03:57:14 +0700

    debug

--- debug.py
+++ debug.py
@@ -1,8 +1,8 @@
-import torch 
-import torch.nn as nn
-from pytorch_forecasting.models.temporal_fusion_transformer.sub_modules import TimeDistributed
-inputs = [1,2,3,4,5,6]
+# import torch 
+# import torch.nn as nn
+# from pytorch_forecasting.models.temporal_fusion_transformer.sub_modules import TimeDistributed
+# inputs = [1,2,3,4,5,6]
 
-out_proj = nn.Linear(6,2)
+# out_proj = nn.Linear(3,2)
 
-tD = 
+# tD = 
